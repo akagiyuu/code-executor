ce00d8527747fce9c4879f72b6784e181dc3db04
akagiyuu <khanghuynh0245@gmail.com>
2025-05-09 23:35:00 +0700

    feat(test): add should timeout test

---
+++ tests/should_timeout.rs
@@ -0,0 +1,61 @@
+use std::time::Duration;
+
+use assert_matches::assert_matches;
+use code_executor::*;
+use rstest::rstest;
+
+fn get_timeout_code(language: Language<'static>) -> &'static str {
+    match language {
+        CPP => {
+            r#"
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    while(true) {}
+}
+        "#
+        }
+        RUST => {
+            r#"
+fn main() {
+    loop {}
+}
+        "#
+        }
+        JAVA => {
+            r#"
+class Main {
+    public static void main(String[] args) {
+        while(true) {}
+    }
+}
+        "#
+        }
+        PYTHON => {
+            r#"
+while True:
+    continue
+        "#
+        }
+        _ => unreachable!(),
+    }
+}
+
+#[rstest]
+#[tokio::test]
+async fn should_output_correct(#[values(CPP, RUST, JAVA, PYTHON)] language: Language<'static>) {
+    let code = get_timeout_code(language);
+    let project_path = language.compiler.compile(code.as_bytes()).await.unwrap();
+
+    let runner = Runner::new(
+        language.runner_args,
+        &project_path,
+        Duration::from_secs(2),
+        i64::MAX,
+    )
+    .unwrap();
+
+    assert_matches!(runner.run(b"").await, Err(Error::Timeout));
+}

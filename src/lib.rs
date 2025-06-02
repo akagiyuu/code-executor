16510112749d0b9fdcdf5e3cde017135e32e91f7
akagiyuu <khanghuynh0245@gmail.com>
2025-06-02 16:20:13 +0700

    feat(unit-test): util function for testing

--- src/lib.rs
+++ src/lib.rs
@@ -22,3 +22,37 @@ pub struct Language<'a> {
     pub compiler: Compiler<'a>,
     pub runner_args: CommandArgs<'a>,
 }
+
+#[cfg(test)]
+mod test {
+    use std::{fs, path::Path};
+
+    use crate::Language;
+
+    pub fn read_test_cases(problem_path: &Path) -> Vec<(Vec<u8>, Vec<u8>)> {
+        let test_cases: Vec<_> = fs::read_dir(problem_path)
+            .unwrap()
+            .filter_map(|entry| {
+                let entry = entry.unwrap();
+                let path = entry.path();
+                if !path.is_dir() {
+                    return None;
+                }
+
+                let input_path = path.join("in.txt");
+                let input = fs::read(input_path).unwrap();
+
+                let output_path = path.join("out.txt");
+                let output = fs::read(output_path).unwrap();
+
+                Some((input, output))
+            })
+            .collect();
+
+        test_cases
+    }
+
+    pub fn read_code(language: Language, problem_path: &Path) -> Vec<u8> {
+        fs::read(problem_path.join(language.compiler.main_file)).unwrap()
+    }
+}
